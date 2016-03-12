a3776b2d4f35852df6f9525b68535a3a128099cf
Sez Prouting <sprout3@gmail.com>
2016-03-13 10:59:14 +1300

    Created Empty client classes

---
+++ src/client/Client.java
@@ -0,0 +1,19 @@
+package client;
+
+/**
+ *  DMS S1 2016 ASSIGNMENT 1
+ *  Kamal Lamgade & Sez Prouting
+ *
+ * 
+ * 
+ * @author 
+ */
+
+import messages.*;
+/**
+ *
+ * @author carmelsez
+ */
+public class Client {
+    
+}
---
+++ src/client/ClientGUI.java
@@ -0,0 +1,20 @@
+package client;
+
+/**
+ *  DMS S1 2016 ASSIGNMENT 1
+ *  Kamal Lamgade & Sez Prouting
+ *
+ * 
+ * 
+ * @author sez
+ */
+
+import messages.*;
+
+/**
+ *
+ * @author carmelsez
+ */
+public class ClientGUI {
+    
+}
